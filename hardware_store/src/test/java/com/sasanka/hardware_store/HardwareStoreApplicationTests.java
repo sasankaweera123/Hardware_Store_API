32377cae5b19828e9f2d27f69e769f3c17aea8a4
Sasanka <sajewsasanka1@gmail.com>
2023-06-27 12:19:11 +0530

    Basic Setup Created

---
+++ hardware_store/src/test/java/com/sasanka/hardware_store/HardwareStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sasanka.hardware_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HardwareStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
