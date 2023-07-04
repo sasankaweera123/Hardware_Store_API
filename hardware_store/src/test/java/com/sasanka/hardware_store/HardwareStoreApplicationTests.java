3c41b0b67e18e852d80df0e192892f6f7b007233
Sasanka <sajewsasanka1@gmail.com>
2023-07-04 22:18:56 +0530

    Update HardwareStoreApplicationTests.java

--- hardware_store/src/test/java/com/sasanka/hardware_store/HardwareStoreApplicationTests.java
+++ hardware_store/src/test/java/com/sasanka/hardware_store/HardwareStoreApplicationTests.java
@@ -3,7 +3,7 @@ package com.sasanka.hardware_store;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class HardwareStoreApplicationTests {
 
 	@Test
