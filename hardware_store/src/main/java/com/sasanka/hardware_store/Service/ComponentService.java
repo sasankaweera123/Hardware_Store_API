0ae89fb7ace8744fbd1011b84ad34a89eac06d33
Sasanka <sajewsasanka1@gmail.com>
2023-07-06 09:12:04 +0530

    Update ComponentService.java
    - delete by ID
    - get by ID
    - update by ID

--- hardware_store/src/main/java/com/sasanka/hardware_store/Service/ComponentService.java
+++ hardware_store/src/main/java/com/sasanka/hardware_store/Service/ComponentService.java
@@ -17,4 +17,34 @@ public class ComponentService {
         return componentRepository.findAll();
     }
 
+    public void deleteComponentById(String componentId) {
+        boolean exists = componentRepository.findByComponentId(componentId).isPresent();
+        if (!exists){
+            throw new IllegalStateException("Component with id " + componentId + " does not exist");
+        }
+        componentRepository.deleteById(componentId);
+    }
+
+    public Component getComponentById(String componentId) {
+        return componentRepository.findByComponentId(componentId).orElseThrow(() -> new IllegalStateException("Component with id " + componentId + " does not exist"));
+    }
+
+    public void addNewComponent(Component component) {
+        boolean exists = componentRepository.findByComponentId(component.getComponentId()).isPresent();
+        if (exists){
+            throw new IllegalStateException("Component with id " + component.getComponentId() + " already exists");
+        }
+        componentRepository.save(component);
+    }
+
+    public void updateComponentById(String componentId, Component component) {
+        boolean exists = componentRepository.findByComponentId(componentId).isPresent();
+        if (!exists){
+            throw new IllegalStateException("Component with id " + componentId + " does not exist");
+        }
+        if(component.getComponentId() != null && component.getComponentId().length() > 0 && !component.getComponentId().equals(componentId)){
+            throw new IllegalStateException("Component id cannot be changed");
+        }
+        componentRepository.save(component);
+    }
 }
