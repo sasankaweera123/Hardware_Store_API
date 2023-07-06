7d0449656cfc40a057561418e084e5d53b37ee33
Sasanka <sajewsasanka1@gmail.com>
2023-07-06 09:12:35 +0530

    Update ComponentController.java
    - delete by ID DELETE
    - get by ID - GET
    - update by ID  - PUT

--- hardware_store/src/main/java/com/sasanka/hardware_store/controller/ComponentController.java
+++ hardware_store/src/main/java/com/sasanka/hardware_store/controller/ComponentController.java
@@ -3,9 +3,7 @@ package com.sasanka.hardware_store.controller;
 import com.sasanka.hardware_store.Document.Component;
 import com.sasanka.hardware_store.Service.ComponentService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,6 +17,27 @@ public class ComponentController {
     @GetMapping("/all")
     public List<Component> getAllComponents() {
         return componentService.getAllComponents();
+    }
+
+    @GetMapping(path = "{componentId}")
+    public Component getComponentById(@PathVariable("componentId") String componentId) {
+        return componentService.getComponentById(componentId);
+    }
+
+    @PostMapping
+    public void addNewComponent(@RequestBody Component component) {
+        componentService.addNewComponent(component);
+    }
+
+    @DeleteMapping(path = "{componentId}")
+    public void deleteComponentById(@PathVariable("componentId") String componentId) {
+        componentService.deleteComponentById(componentId);
+    }
+
+    @PutMapping(path = "{componentId}")
+    public void updateComponentById(@PathVariable("componentId") String componentId,
+                                    @RequestBody Component component) {
+        componentService.updateComponentById(componentId, component);
     }
 
 }
