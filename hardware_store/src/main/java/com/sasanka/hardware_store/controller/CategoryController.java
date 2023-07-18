eb925e28e3c8d24c3799f593e8eb8e9ef21dee07
Sasanka <sajewsasanka1@gmail.com>
2023-07-18 11:53:13 +0530

    Controller update
    - Controllers Update According to the Custom Response

--- hardware_store/src/main/java/com/sasanka/hardware_store/controller/CategoryController.java
+++ hardware_store/src/main/java/com/sasanka/hardware_store/controller/CategoryController.java
@@ -1,15 +1,17 @@
 package com.sasanka.hardware_store.controller;
 
+import com.sasanka.hardware_store.model.Response;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sasanka.hardware_store.Service.CategoryService;
-import com.sasanka.hardware_store.Document.Category;
+import static org.springframework.http.HttpStatus.OK;
 
-import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/category")
@@ -19,13 +21,26 @@ public class CategoryController {
     private final CategoryService categoryService;
 
     @GetMapping("/all")
-    public List<Category> getAllCategories() {
-        return categoryService.getAllCategories();
+    public ResponseEntity<Response> getAllCategories() {
+
+        return ResponseEntity.ok(Response.builder()
+                .timestamp(java.time.LocalDateTime.now())
+                .statusCode(200)
+                .status(OK)
+                .message("Categories fetched successfully")
+                .data(Map.of("categories", categoryService.getAllCategories()))
+                .build());
     }
 
     @GetMapping(path = "{categoryId}")
-    public Category getCategoryById(@PathVariable("categoryId") String categoryId) {
-        return categoryService.getCategoryById(categoryId);
+    public ResponseEntity<Response> getCategoryById(@PathVariable("categoryId") String categoryId) {
+        return ResponseEntity.ok(Response.builder()
+                .timestamp(java.time.LocalDateTime.now())
+                .statusCode(200)
+                .status(OK)
+                .message("Category fetched successfully")
+                .data(Map.of("category", categoryService.getCategoryById(categoryId)))
+                .build());
     }
 
 }
--- hardware_store/src/main/java/com/sasanka/hardware_store/controller/StoreController.java
+++ hardware_store/src/main/java/com/sasanka/hardware_store/controller/StoreController.java
@@ -2,7 +2,9 @@ package com.sasanka.hardware_store.controller;
 
 import com.sasanka.hardware_store.Document.Store;
 import com.sasanka.hardware_store.Service.StoreService;
+import com.sasanka.hardware_store.model.Response;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,12 +20,25 @@ public class StoreController {
     private final StoreService storeService;
 
     @GetMapping("/all")
-    public List<Store> getAllStores() {
-        return storeService.getAllStores();
+    public ResponseEntity<Response> getAllStores() {
+
+        return ResponseEntity.ok(Response.builder()
+                .timestamp(java.time.LocalDateTime.now())
+                .statusCode(200)
+                .status(org.springframework.http.HttpStatus.OK)
+                .message("Stores fetched successfully")
+                .data(java.util.Map.of("stores", storeService.getAllStores()))
+                .build());
     }
 
     @GetMapping(path = "{storeId}")
-    public Store getStoreById(@PathVariable("storeId") String storeId) {
-        return storeService.getStoreById(storeId);
+    public ResponseEntity<Response> getStoreById(@PathVariable("storeId") String storeId) {
+        return ResponseEntity.ok(Response.builder()
+                .timestamp(java.time.LocalDateTime.now())
+                .statusCode(200)
+                .status(org.springframework.http.HttpStatus.OK)
+                .message("Store fetched successfully")
+                .data(java.util.Map.of("store", storeService.getStoreById(storeId)))
+                .build());
     }
 }
